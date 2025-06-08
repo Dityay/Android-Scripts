9b7402b4d337b591691819e559b7a6c855f6cee9
Dtyaa <100247109+Dityay@users.noreply.github.com>
2025-06-08 19:26:34 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,98 +1,40 @@
 #!/bin/bash
-# crave run --no-patch -- "curl https://raw.githubusercontent.com/tillua467/Android-Scripts/refs/heads/main/script.sh | bash"
+# crave run --no-patch -- "curl https://raw.githubusercontent.com/Dityay/Android-Scripts/refs/heads/main/script.sh | bash"
 
-# Remove Unnecessary Files
-echo "===================================="
-echo "     Removing Unnecessary Files"
-echo "===================================="
+# R#!/bin/bash
 
-dirs_to_remove=(
-  "vendor/xiaomi"
-  "kernel/xiaomi"
-  "device/xiaomi"
-  "device/xiaomi/sm6150-common"
-  "vendor/xiaomi/sm6150-common"
-  "hardware/xiaomi"
-  "out/target/product/*/*zip"
-  "out/target/product/*/*txt"
-  "out/target/product/*/boot.img"
-  "out/target/product/*/recovery.img"
-  "out/target/product/*/super*img"
-)
+rm -rf .repo/local_manifests/
 
-for dir in "${dirs_to_remove[@]}"; do
-  [ -e "$dir" ] && rm -rf "$dir"
-done
+# repo init rom
+repo init -u https://github.com/DerpFest-LOS/android_manifest.git -b 15.2 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
 
-echo "===================================="
-echo "  Removing Unnecessary Files Done"
-echo "===================================="
+# Local manifests
+git clone https://github.com/Dityay/local_manifests -b witaqua .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
-# Initialize repo
-echo "=============================================="
-echo "         Cloning Manifest..........."
-echo "=============================================="
-if ! repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs; then
-  echo "Repo initialization failed."
-fi
-echo "=============================================="
-echo "       Manifest Cloned successfully"
-echo "=============================================="
-# Sync
-if ! /opt/crave/resync.sh || ! repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all); then
-  echo "Repo sync failed."
-fi
-echo "============="
-echo " Sync success"
-echo "============="
-
-# Clone device trees and other dependencies
-echo "=============================================="
-echo "       Cloning Trees..........."
-echo "=============================================="
-rm -rf device/xiaomi
-
-rm -rf vendor/xiaomi
-
-rm -rf kernel/xiaomi
-
-rm -rf hardware/xiaomi
-
-rm -rf hardware/mediatek
-
-rm -rf device/mediatek/sepolicy_vndr
-
-git clone https://github.com/Dityay/android_device_xiaomi_earth.git -b lineage-22.2 device/xiaomi/earth || { echo "Failed to clone device tree"; }
-
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth || { echo "Failed to clone vendor tree"; }
-
-git clone https://github.com/Dityay/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth || { echo "Failed to clone kernel tree"; }
-
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi || { echo "Failed to clone xiaomi stuffs"; }
-
-git clone https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek || { echo "Failed to clone mediatek hardwares"; }
-
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr || { echo "Failed to sepolicy_vndr"; }
-
+# build
 /opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
 
-# Export Environment Variables
-echo "======= Exporting........ ======"
-export BUILD_USERNAME=Dityay
+# Signing keys
+git clone https://github.com/AbuRider/vendor_signing.git vendor/lineage/signing/keys
+
+# Export
+export BUILD_USERNAME=deltarune
 export BUILD_HOSTNAME=crave
-export TZ=Asia/Makassar
-export ALLOW_MISSING_DEPENDENCIES=true
-echo "======= Export Done ======"
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
-# Set up build environment
-echo "====== Starting Envsetup ======="
-source build/envsetup.sh || { echo "Envsetup failed"; exit 1; }
-echo "====== Envsetup Done ======="
-
-
-# Build ROM
-echo "===================================="
-echo "  BRINGING TO HORIZON , STARTING BUILD.."
-echo "===================================="
+# initiate build setup
 . build/envsetup.sh
+
+
+echo "======= Export Done ======"
 brunch earth
+#lunch lineage_earth-bp1a-userdebug && mka derp
